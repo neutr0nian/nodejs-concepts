4dd8557502784e735d8e08d9c10a68f0e0e5959c
Pratik Chavan <chavanpratik.pc@gmail.com>
2023-02-06 15:20:56 -0500

    pushing local work

---
+++ controllers/homeController.js
@@ -0,0 +1,26 @@
+let courses = [
+  {
+    title: "Event Driven Cakes",
+    cost: 50,
+  },
+  {
+    title: "Asynchronous Artichoke",
+    cost: 25,
+  },
+  {
+    title: "Object Oriented Orange Juice",
+    cost: 10,
+  },
+];
+
+module.exports = {
+  showCourses: (req, res) => {
+    res.render("courses", { offeredCourses: courses });
+  },
+  showSignUp: (req, res) => {
+    res.render("contact");
+  },
+  postedSignUpForm: (req, res) => {
+    res.render("thanks");
+  },
+};
